95f4e6ee510eea07d6faec4cfb2eb48db275c056
edisonfang <edisonfang@edisonfangdeMacBook-Air.local>
2017-06-23 16:54:50 +0800

    git@github.com:yunhsuan1010/RRRRRRR.git

--- 擇偶/ViewController.swift
+++ 擇偶/ViewController.swift
@@ -15,6 +15,48 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var bmi: UITextField!
+    @IBOutlet weak var stars: UITextField!
+    @IBOutlet weak var blood: UISegmentedControl!
+    @IBOutlet weak var no: UIImageView!
+    @IBOutlet weak var yess: UIImageView!
+    @IBAction func buttom(_ sender: Any) {
+        if bmi.text == "20"{
+            if stars.text == "ss"{
+                if blood.selectedSegmentIndex == 2 {
+                no.isHidden = true
+        yess.isHidden  = false
+                    
+                } else {
+                no.isHidden = false
+                    yess.isHidden = true
+                    
+                
+                }
+            
+            }else {
+                no.isHidden = false
+                yess.isHidden = true
+                
+                
+            }
+        
+        
+        }else {
+            no.isHidden = false
+            yess.isHidden = true
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+    }
+    @IBOutlet weak var yes: UIImageView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
