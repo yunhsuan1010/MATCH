c74bc15219fcd40ca484901d2b5304c8751c3b33
edisonfang <edisonfang@edisonfangdeMacBook-Air.local>
2017-06-23 16:21:26 +0800

    Initial Commit

---
+++ 擇偶/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  擇偶
+//
+//  Created by edisonfang on 2017/6/23.
+//  Copyright © 2017年 hsuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
